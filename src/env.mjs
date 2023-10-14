1227b9d85eff2e0f5f33ab66dd4d2078fbedab73
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-10-14 15:37:34 +0100

    updated environment variables

--- src/env.mjs
+++ src/env.mjs
@@ -20,9 +20,6 @@ export const env = createEnv({
 
     // clerk keys;
     CLERK_SECRET_KEY: z.string(),
-    // Add ` on ID and SECRET if you want to make sure they're not empty
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
   },
 
   /**
@@ -51,8 +48,6 @@ export const env = createEnv({
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
