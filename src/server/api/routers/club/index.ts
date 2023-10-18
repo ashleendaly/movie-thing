6143cb6d6ed4168954c14d0d9cdbc5afb7569f70
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-10-18 17:03:51 +0100

    added get clubs by user

--- src/server/api/routers/club/index.ts
+++ src/server/api/routers/club/index.ts
@@ -1,4 +1,4 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { adminRouter } from "./admin";
 import { memberRouter } from "./members";
 
@@ -10,4 +10,13 @@ import { memberRouter } from "./members";
 export const clubRouter = createTRPCRouter({
   admin: adminRouter,
   members: memberRouter,
+  getForUser: protectedProcedure.query(async ({ ctx }) => {
+    const userID = ctx.auth.userId;
+    return await ctx.db
+      .selectFrom("Club")
+      .innerJoin("ClubMembership", "Club.ID", "ClubMembership.clubID")
+      .where("ClubMembership.userID", "==", userID)
+      .select(["Club.ID", "Club.name"])
+      .execute();
+  }),
 });
