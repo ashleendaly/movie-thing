6f87d5016a3782669c98c8df04242ff3810083bf
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-10-26 23:04:53 +0100

    refactored watchlist/add page to be slightly better looking

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,16 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default config;
