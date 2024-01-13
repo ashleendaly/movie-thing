5d4106cd7c3464c11e5dbe90d67a756fbb190e0d
Petros <93052465+pkitazos@users.noreply.github.com>
2024-01-13 16:57:09 +0000

    closed dialog after movie is added to user watchlist

--- src/app/watchlist/add/addable-movie.tsx
+++ src/app/watchlist/add/addable-movie.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import { toast } from "sonner";
 import { MovieListing } from "~/components/movie-listing";
 import { Button } from "~/components/ui/button";
@@ -12,38 +13,41 @@ import { api } from "~/lib/trpc/react";
 import { type Movie } from "~/types/db";
 
 export const AddableMovie = ({ movie }: { movie: Movie }) => {
+  const [open, setOpen] = useState(false);
+
   const { mutateAsync } = api.watchList.add.useMutation();
   return (
-    <>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button className="h-16 w-full" size="lg">
-            <MovieListing movie={movie} />
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="h-[70dvh]">
-          <div className="grid">
-            <div>Add to watchlist?</div>
-            <Button
-              variant="accent"
-              onClick={() =>
-                toast.promise(mutateAsync({ ...movie }), {
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="h-16 w-full" size="lg">
+          <MovieListing movie={movie} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="h-[70dvh]">
+        <div className="grid">
+          <div>Add to watchlist?</div>
+          <Button
+            variant="accent"
+            onClick={() =>
+              toast.promise(
+                mutateAsync({ ...movie }).then(() => setOpen(false)),
+                {
                   loading: "loading",
                   success: "Added movie to watchlist!",
                   error: "Something went wrong",
-                })
-              }
-            >
-              yes
+                },
+              )
+            }
+          >
+            yes
+          </Button>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              no
             </Button>
-            <DialogClose asChild>
-              <Button type="button" variant="secondary">
-                no
-              </Button>
-            </DialogClose>
-          </div>
-        </DialogContent>
-      </Dialog>
-    </>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
