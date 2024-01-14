e15e8f6da17758f94bea5e5630cc1b7ecc17743e
Petros <93052465+pkitazos@users.noreply.github.com>
2024-01-14 13:04:02 +0000

    updated add movie dialog with card component

---
+++ src/app/watchlist/add/add-movie-dialog.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { toast } from "sonner";
+import { MovieListing } from "~/components/movie-listing";
+import { Button } from "~/components/ui/button";
+import { Card, CardHeader, CardTitle } from "~/components/ui/card";
+import { DialogClose } from "~/components/ui/dialog";
+import { api } from "~/lib/trpc/react";
+import { type Movie } from "~/types/db";
+
+export function AddMovieDialog({
+  movie,
+  closeDialog,
+}: {
+  movie: Movie;
+  closeDialog?: () => void;
+}) {
+  const { mutateAsync } = api.watchList.add.useMutation();
+  return (
+    // TODO: fix background of card, currently overflows
+    <Card className="grid grid-cols-2 place-items-center gap-3 border-x-thick border-y-thick border-primary px-7 py-4 ">
+      <CardHeader className="col-span-2 text-2xl text-foreground underline decoration-accent decoration-4 underline-offset-2">
+        <CardTitle>Add to Watchlist?</CardTitle>
+      </CardHeader>
+      <MovieListing
+        movie={movie}
+        className="col-span-2 w-2/3 place-self-center rounded-sm"
+      />
+
+      <Button
+        variant="constructive"
+        className="w-full font-semibold"
+        size="lg"
+        onClick={() =>
+          toast.promise(mutateAsync({ ...movie }).then(closeDialog), {
+            loading: "loading",
+            success: "Added movie to watchlist!",
+            error: "Something went wrong",
+          })
+        }
+      >
+        yes
+      </Button>
+      <DialogClose asChild>
+        <Button
+          className="w-full font-semibold"
+          type="button"
+          variant="destructive"
+          size="lg"
+        >
+          no
+        </Button>
+      </DialogClose>
+    </Card>
+  );
+}
--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -1,18 +1,21 @@
 import { MovieListing } from "~/components/movie-listing";
+import { Dialog } from "~/components/ui/dialog";
 import { ScrollArea } from "~/components/ui/scroll-area";
 import { api } from "~/lib/trpc/server";
 
 const WatchList = async () => {
   const movies = await api.watchList.getForUser.query();
   return (
-    <div>
-      <ScrollArea className="h-[80dvh]">
-        <ul className="grid grid-cols-2 gap-10  p-5">
-          {movies.map((movie) => (
-            <MovieListing key={movie.imdbID} movie={movie} />
-          ))}
-        </ul>
-      </ScrollArea>
+    <div className="m-7">
+      <Dialog>
+        <ScrollArea className="h-[80dvh]">
+          <ul className="grid grid-cols-2 gap-10 p-5">
+            {movies.map((movie) => (
+              <MovieListing key={movie.imdbID} movie={movie} />
+            ))}
+          </ul>
+        </ScrollArea>
+      </Dialog>
     </div>
   );
 };
