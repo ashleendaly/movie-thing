01893ea88094995734bcd7d40766109014a35e57
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-01-29 21:05:30 +0000

    setup basic python stuff

--- src/apy/app.py
+++ src/apy/app.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/apy")
 def hello_world():
     return {"message": "Hello World"}
+
+
+@app.get("/aggregate")
+def aggregate():
+
+    # load ranking data
+
+    # run aggregation
+    # return results
+    return {"results": [1,2,3]}
