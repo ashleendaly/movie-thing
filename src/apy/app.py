ab46b1922a17d758cb33deb4a1383f6bfcd00494
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-01-31 22:48:15 +0000

    removed unneeded endpoints

--- src/apy/app.py
+++ src/apy/app.py
@@ -1,4 +1,3 @@
-from typing import List
 from fastapi import FastAPI, Request
 
 
@@ -10,18 +9,8 @@ app = FastAPI()
 app.state.sql_repo = sql_repo
 
 
-@app.get("/apy")
-def hello_world():
-    return {"message": "Hello World"}
-
-
 @app.get("/aggregate/{clubName}")
 def aggregate(clubName: str, request: Request):
     data = request.app.state.sql_repo.get_user_preferences(clubName)
     results = aggregate_borda(data)
     return request.app.state.sql_repo.write_results(results)
-
-
-@app.get("/clubmembers/{clubName}")
-def get_club_members(clubName: str, request: Request) -> List[str]:
-    return request.app.state.sql_repo.get_club_members(clubName)
