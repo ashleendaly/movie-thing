d4d23a6a4393974a447a4551c0b771dd8681230f
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-02-01 14:38:31 +0000

    added test route

--- api/index.py
+++ api/index.py
@@ -14,3 +14,8 @@ def aggregate(clubName: str, request: Request):
     data = request.app.state.sql_repo.get_user_preferences(clubName)
     results = aggregate_borda(data)
     return request.app.state.sql_repo.write_results(clubName, results)
+
+
+@app.get("/api/test")
+def hello():
+    return "hello world"
