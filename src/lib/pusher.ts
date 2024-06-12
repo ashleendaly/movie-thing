06dd720ef37b4dff93c3c5c93b4d37952904cf8a
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-06-12 17:49:46 +0100

    fixed it

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -2,14 +2,14 @@ import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 import { env } from "~/env";
 
-export const pusherClient = new PusherClient(env.NEXT_PUBLIC_PUSHER_KEY, {
+export const pusherClient = new PusherClient(env.NEXT_PUBLIC_PUSHER_APP_ID, {
   cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
 });
 
 export const pusherServer = new PusherServer({
   appId: env.NEXT_PUBLIC_PUSHER_APP_ID,
-  key: env.NEXT_PUBLIC_PUSHER_KEY,
-  secret: env.NEXT_PUBLIC_PUSHER_SECRET,
+  key: env.PUSHER_KEY,
+  secret: env.PUSHER_SECRET,
   cluster: env.NEXT_PUBLIC_PUSHER_CLUSTER,
   useTLS: true,
 });
