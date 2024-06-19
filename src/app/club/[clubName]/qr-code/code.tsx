c0015a100c013f8d63f974d551164a3db475946a
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2024-06-19 17:27:05 +0100

    fixed QR Code sizing

--- src/app/club/[clubName]/qr-code/code.tsx
+++ src/app/club/[clubName]/qr-code/code.tsx
@@ -14,11 +14,25 @@ export function Code({
     <div className={cn(className)}>
       <p className="pb-3 text-sm italic underline">Join this club:</p>
       <QRCodeSVG
-        className="border-8 border-solid border-primary p-5"
+        className="block border-8 border-solid border-primary p-5 lg:hidden 2xl:block"
         value={`${env.NEXT_PUBLIC_OUR_URL}/join/${joinCode}`}
         bgColor="hsl(var(--background))"
         fgColor="hsl(var(--foreground))"
-        size={256}
+        size={240}
+      />
+      <QRCodeSVG
+        className="hidden border-8 border-solid border-primary p-5 xl:block 2xl:hidden"
+        value={`${env.NEXT_PUBLIC_OUR_URL}/join/${joinCode}`}
+        bgColor="hsl(var(--background))"
+        fgColor="hsl(var(--foreground))"
+        size={200}
+      />
+      <QRCodeSVG
+        className="hidden border-8 border-solid border-primary p-5 lg:block xl:hidden"
+        value={`${env.NEXT_PUBLIC_OUR_URL}/join/${joinCode}`}
+        bgColor="hsl(var(--background))"
+        fgColor="hsl(var(--foreground))"
+        size={150}
       />
     </div>
   );
